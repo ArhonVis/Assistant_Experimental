1cb65c6ce1244d20e92a103badccb0351a21522a
ArhonVis <ant0nidas913@gmail.com>
2021-10-28 18:40:08 +0300

    Fix imports in api

--- api/ApiConfig.py
+++ api/ApiConfig.py
@@ -1,6 +1,9 @@
-import Assistant_Experimental.config.SysConf as S_config
-import Assistant_Experimental.config.libConfig as Lib_conf
-import Assistant_Experimental.config.helpConfig as Help_conf
+from pathlib import Path
+import sys
+sys.path.append(Path(__file__).parents[1].absolute())
+import config.SysConf as S_config
+import config.libConfig as Lib_conf
+import config.helpConfig as Help_conf
 
 System = S_config.System
 Libraries = {'tele_bot': Lib_conf.bot_conf}
